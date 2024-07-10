dd4c390572e49a2849dabbe2e4032684cc1fad26
DemomicVenom <venomaayan4@gmail.com>
2024-07-10 23:10:19 +0530

    huhd ewhoi3h  3rh389   83yr8ty

---
+++ src/Components/ProductDisplay/ProductDisplay.jsx
@@ -0,0 +1,57 @@
+import React from 'react'
+import './ProductDisplay.css'
+import star_icon from '../Assets/star_icon.png'
+import star_dull_icon from '../Assets/star_dull_icon.png'
+
+const ProductDisplay = (props) => {
+  const {product} = props;
+  return (
+    <div className='productdisplay'>
+      <div className="productdisplay-left">
+        <div className="productdisplay-img-list">
+          <img src={product.image} alt="" />
+          <img src={product.image} alt="" />
+          <img src={product.image} alt="" />
+          <img src={product.image} alt="" />
+        </div>
+        <div className="productdisplay-img">
+          <img className='productdisplay-main-img' src={product.image} alt="" />
+        </div>
+
+      </div>
+      <div className="productdisplay-right">
+        <h1>{product.name}</h1>
+        <div className="productdisplay-right-star">
+        <img src={star_icon} alt="" />
+          <img src={star_icon} alt="" />
+          <img src={star_icon} alt="" />
+          <img src={star_icon} alt="" />
+          <img src={star_dull_icon} alt="" />
+          <p>(122)</p>
+        </div>
+        <div className="productdisplay-right-prices">
+          <div className="productdisplay-right-price-old">${product.old_price} </div>
+          <div className="productdisplay-right-price-new">${product.new_price}</div>
+        </div>
+        <div className="productdisplay-right-description">
+        Designed for the jet-setter in mind, our shirt is the ideal blend of comfort and style. Built for the man who’s in demand, this classic design will keep you looking presentable on the go, with a gentle fabric for all-day comfort.
+        </div>
+        <div className="productdisplay-right-size">
+          <h1>Select Size</h1>
+          <div className="productdisplay-right-sizes">
+            <div>S</div>
+            <div>M</div>
+            <div>L</div>
+            <div>XL</div>
+            <div>XXL</div>
+          </div>
+        </div>
+        <button>ADD TO CART</button>
+        <p className='productdisplay-right-category'><span>Category :</span>Women , T-Shirt, Crop Top</p>
+        <p className='productdisplay-right-category'><span>Tags :</span>Modern , Latest</p>
+      </div>
+    </div>
+  )
+}
+
+export default ProductDisplay
